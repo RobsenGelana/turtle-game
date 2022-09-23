6cebf7780c7bb91e160e6bea047efbd27e9cccb9
RobsenGelana <robsengelana21@gmail.com>
2022-09-23 22:50:03 +0300

    Creating the player class for the game

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from turtle import Screen
+import time
+from player import Player
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor('white')
+screen.tracer(0)
+
+player = Player()
 
 
 
 
 
 
-
-
-
-
-
+game_on = True
+while game_on:
+    time.sleep(0.1)
+    screen.update()
 
 
 
