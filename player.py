235199b0989316c645a50349877309f4041be5b3
RobsenGelana <robsengelana21@gmail.com>
2022-09-24 10:10:56 +0300

    creating a player function

--- player.py
+++ player.py
@@ -4,14 +4,24 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+
 class Player(Turtle):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
         self.shape('turtle')
         self.color('black')
         self.penup()
         self.goto(STARTING_POSITION)
         self.left(90)
-    
-    def move_up(self):
+        
+
+    def move_forward(self):
         self.forward(MOVE_DISTANCE)
+    
+    def got_to_start(self):
+        self.goto(STARTING_POSITION)
+       
+    def is_at_finish(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        return False
