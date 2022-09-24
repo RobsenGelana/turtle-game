7d41bc1d1864dad5c68155b5f8fe28afaafc0009
RobsenGelana <robsengelana21@gmail.com>
2022-09-24 10:11:54 +0300

    finishing the game

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-from turtle import Screen
 import time
+from turtle import Screen
 from player import Player
 from carmanagment import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
 screen.tracer(0)
-
 player = Player()
-car_manager = CarManager
+car_manager = CarManager()
+score_board = Scoreboard()
 
 screen.listen()
-screen.onkey(player.move_up, 'Up')
+screen.onkey(player.move_forward, 'Up')
 
-
-
-game_on = True
-while game_on:
+game_is_on = True
+while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-
-
-
-
-
-
-
-
+    car_manager.create_car()
+    car_manager.move_cars()
+    
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score_board.game_over()
+    
+    if player.is_at_finish():
+        player.got_to_start()
+        car_manager.level_up()
+        score_board.increase_level()
 
 screen.exitonclick()
